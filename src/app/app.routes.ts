a3f15dd76d18f17dfd2189576b8679e120380cf3
Laura Moreno <chipifsdev@gmail.com>
2025-06-04 19:06:04 +0200

    creadas las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { MovieDetail } from './pages/movie-detail/movie-detail';
+import { Search } from './pages/search/search';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', component: Home},
+    {path: 'movie/:id', component: MovieDetail},
+    {path: 'search', component: Search},
+
+];
