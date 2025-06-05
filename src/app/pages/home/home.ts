8ba0cd062e1f5f5b24ce8406b5f967c609ba2a68
Laura Moreno <chipifsdev@gmail.com>
2025-06-05 12:39:29 +0200

    quitado el .env y pasado al back // provideHttpClient // ngOnInit de la home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class Home {
 
+  peliculas: any[] = []
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:3000/api/peliculas')
+    .subscribe({
+      next: res => {
+        this.peliculas = res.results;
+      },
+      error: err => {
+        console.error('Error al cargar las peliculas:', err);
+      }
+    })
+  }
 }
