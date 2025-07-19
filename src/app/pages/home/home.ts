354e96b2001d3fe4b7b8acd4b92dedec2d52a031
Laura Moreno <chipifsdev@gmail.com>
2025-07-19 20:24:06 +0200

    borrado el componente movie-card que no voy a usar y puesto los estilos de los botones de la home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,24 +1,54 @@
+import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  movies: any[] = [];
+  series: any[] = [];
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
-    // this.cargarSeries();
+    this.getPeliculas();
+    this.getSeries();
   }
 
-//   cargarSeries() {
-//   this.http.get<any>(`http://localhost:3000/api/series?page=${this.page}`)
-//     .subscribe({
-//       next: res => this.series = res.results,
-//       error: err => console.error('Error al cargar las series:', err)
-//     });
-// }
+  getPeliculas() {
+    this.http.get<any>('http://localhost:3000/api/peliculas?page=1')
+      .subscribe({
+        next: res => this.movies = res.results.slice(0, 20),
+        error: err => console.error('Error al cargar películas:', err)
+      });
+  }
 
+  getSeries() {
+    this.http.get<any>('http://localhost:3000/api/series?page=1')
+      .subscribe({
+        next: res => this.series = res.results.slice(0, 20),
+        error: err => console.error('Error al cargar series:', err)
+      });
+  }
 
+  verMasPeliculas() {
+    this.router.navigate(['/peliculas']);
+  }
+
+  verMasSeries() {
+    this.router.navigate(['/series']);
+  }
+
+  verDetalle(id: number, tipo: 'pelicula' | 'serie') {
+    if (tipo === 'pelicula') {
+      this.router.navigate(['/movie', id]);
+    } else {
+      this.router.navigate(['/serie', id]);
+    }
+  }
 }
